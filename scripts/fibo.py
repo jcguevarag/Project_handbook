f5dc3b4fac2e140a38012b767db83cd6b63b6561
JCGG <juancamilo.guevaragomez@gmail.com>
2023-02-08 15:19:06 +0100

    Local cache to avoid large comp

--- scripts/fibo.py
+++ scripts/fibo.py
@@ -1,4 +1,31 @@
-def fibo(n):
+def fib(x):
+    cache = {}  # Use to avoid running many times
+
+    def fib_inner(x):
+        nonlocal cache
+        if x in cache:
+            return cache[x]
+        if x == 0:
+            return 0
+        elif x == 1:
+            return 1
+        else:
+            val = fib_inner(x - 1) + fib_inner(x - 2)
+            cache[x] = val
+            return val
+    return fib_inner(x)
+
+
+if __name__ == '__main__':
+    assert fib(0) == 0  # assert yields error if false
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(6) == 8
+    assert fib(40) == 102334155
+    print("Tests passed")
+
+
+""" def fibo(n):
     if n < 0:
         print("Incorrect input")
     elif n == 0:
@@ -10,9 +37,10 @@ def fibo(n):
 
 
 if __name__ == '__main__':
-    assert fibo(0) == 0  # assert yields error if false
+    assert fibo(0) == 0
     assert fibo(1) == 1
     assert fibo(2) == 1
     assert fibo(6) == 8
     assert fibo(40) == 102334155
     print("Tests passed")
+"""
