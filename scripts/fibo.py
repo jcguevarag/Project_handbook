3dd2dd6cf67e4296d929c44e08e0693357107ce9
JCGG <juancamilo.guevaragomez@gmail.com>
2023-02-08 15:15:49 +0100

    assert added to fibo

--- scripts/fibo.py
+++ scripts/fibo.py
@@ -3,7 +3,16 @@ def fibo(n):
         print("Incorrect input")
     elif n == 0:
         return 0
-    elif n == 1 or n == 2:
+    elif n == 1:
         return 1
     else:
         return fibo(n-1) + fibo(n-2)
+
+
+if __name__ == '__main__':
+    assert fibo(0) == 0  # assert yields error if false
+    assert fibo(1) == 1
+    assert fibo(2) == 1
+    assert fibo(6) == 8
+    assert fibo(40) == 102334155
+    print("Tests passed")
