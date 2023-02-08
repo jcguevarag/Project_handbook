054609d965440eb27f4e482f03e2def339196d54
JCGG <juancamilo.guevaragomez@gmail.com>
2023-02-08 15:27:30 +0100

    Testing with pytest

--- cbpkguno/fibo.py
+++ cbpkguno/fibo.py
@@ -1,6 +1,7 @@
 def fib(x):
-    cache = {}  # Use to avoid running many times
-
+    if x % 1 != 0 or x < 0:
+        raise NotImplementedError('fib only defined on non-negative integers.')
+    cache = {}
     def fib_inner(x):
         nonlocal cache
         if x in cache:
